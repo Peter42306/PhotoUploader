dd258efd3df27e4606ef6c2bb3a2b6aa24f45a07
Petr Zalizko <pzalizko@gmail.com>
2024-07-20 12:33:56 +0300

    Add project files.

---
+++ PhotoUploader/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PhotoUploader.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
